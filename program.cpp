e1f98264ab411d9f1f8fdb95caac665891f6834c
Marius Urbelis <mariusurbelis23@gmail.com>
2020-02-20 18:51:37 +0000

    Half working data reading code

--- program.cpp
+++ program.cpp
@@ -15,10 +15,11 @@ struct library {
     int signupTime;
     int canBeScanned;
     int bookCount;
+    vector<book> books;
 };
 
 // Declaring the variables
-int bookTypes, libraries, days;
+int bookTypes, librariesCount, days;
 
 int main () {
     // Declaring the ifstream
@@ -27,18 +28,37 @@ int main () {
     file.open("input/a_example.txt");
 
     // Reading in the first line. B, L and D
-    file >> bookTypes >> libraries >> days;
+    file >> bookTypes >> librariesCount >> days;
 
     // Declaring the score array
     int scores [bookTypes];
+    library libraries [librariesCount];
 
     // Reading in the scores
     for (int i = 0; i < bookTypes; i++) {
         file >> scores[i];
     }
 
+    // Reading in the library book counts
+    for (int i = 0; i < librariesCount; i++) {
+        file >> libraries[i].bookCount >> libraries[i].signupTime >> libraries[i].canBeScanned;
+        cout << libraries[i].bookCount << " ";
+    }
+
+    cout << endl << endl;
+
+    for (int l = 0; l < librariesCount; l++) {
+        for (int b = 0; b < libraries[l].bookCount; b++) {
+            book newBook;
+            file >> newBook.id;
+            newBook.score = scores[newBook.id];
+            libraries[l].books.push_back(newBook);
+        }
+    }
+
+
     // Printing out the variables
-    cout << "B: " << bookTypes << " L: " << libraries << " D: " << days << endl;
+    cout << "B: " << bookTypes << " L: " << librariesCount << " D: " << days << endl;
 
     // Closing the file stream
     file.close();

e695d43d20e80f8798428211a4b60a22bb32074e
Marius Urbelis <mariusurbelis23@gmail.com>
2020-02-20 18:05:17 +0000

    Added some code to read in the first line

--- program.cpp
+++ program.cpp
@@ -3,9 +3,24 @@
 
 using namespace std;
 
+// Declaring the variables
+int bookTypes, libraries, days;
+
 int main () {
+    // Declaring the ifstream
+    ifstream file;
+    // Opening a file
+    file.open("input/a_example.txt");
 
-    cout << "hello  world" << endl;
+    // Reading in the first line. B, L and D
+    file >> bookTypes >> libraries >> days;
 
+    // Printing out the variables
+    cout << "B: " << bookTypes << " L: " << libraries << " D: " << days << endl;
+
+    // Closing the file stream
+    file.close();
+
+    // Returning success
     return 0;
 }
